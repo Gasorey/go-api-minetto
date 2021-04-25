51aa093edac15ab8033f7ff46afb502190b6794d
Gabriel Asorey <gasorey@gmail.com>
2021-04-25 10:40:25 -0300

    feat: add base packages and func

--- web/main.go
+++ web/main.go
@@ -1,24 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	ok, err := say("Hello World")
-	if err != nil {
-		panic(err.Error())
-	}
-	switch ok {
-	case true:
-		fmt.Println("Everything Ok")
-	default:
-		fmt.Println("Got an error")
-	}
-}
 
-func say(what string) (bool, error) {
-	if what == "" {
-		return false, fmt.Errorf("empty string")
-	}
-	fmt.Println(what)
-	return true, nil
 }
