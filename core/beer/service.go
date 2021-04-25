b5c2ff05b9f82b54e2c28caa1c92441662917d74
Gabriel Asorey <gasorey@gmail.com>
2021-04-25 12:11:07 -0300

    feat: add service struct

--- core/beer/service.go
+++ core/beer/service.go
@@ -1,1 +1,44 @@
 package beer
+
+type Reader interface {
+	GetAll() ([]*Beer, error)
+	Get(ID int) (*Beer, error)
+}
+
+type Writer interface {
+	Store(b *Beer) error
+	Update(b *Beer) error
+	Remove(ID int) error
+}
+
+type UseCase interface {
+	Reader
+	Writer
+}
+
+type Service struct{}
+
+//This function  will act as a constructor
+func NewService() *Service {
+	return &Service{}
+}
+
+func (s *Service) GetAll() ([]*Beer, error) {
+	return nil, nil
+}
+
+func (s *Service) Get(ID int) (*Beer, error) {
+	return nil, nil
+}
+
+func (s *Service) Store(b *Beer) error {
+	return nil
+}
+
+func (s *Service) Update(b *Beer) error {
+	return nil
+}
+
+func (s *Service) Remove(ID int) error {
+	return nil
+}
